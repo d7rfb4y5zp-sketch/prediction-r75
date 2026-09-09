9974f4739541a456517f6f08cac5500b364757d5
d7rfb4y5zp-sketch <d7rfb4y5zp@privaterelay.appleid.com>
2026-09-09 00:31:08 +0000

    Update config.ts

--- src/components/shared/utils/config/config.ts
+++ src/components/shared/utils/config/config.ts
@@ -105,7 +105,7 @@ export const generateOAuthURL = async (prompt?: string): Promise<string> => {
 
         const config: AuthConfig = {
             clientId,
-            redirectUri: window.location.origin,
+            redirectUri: `${window.location.origin}/prediction-r75/`,
             scopes: 'trade',
         };
 
