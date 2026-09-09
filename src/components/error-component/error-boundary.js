318680a738203b22160a38a9464efce9ab6a1a99
d7rfb4y5zp-sketch <d7rfb4y5zp@privaterelay.appleid.com>
2026-09-09 20:16:50 +0000

    Update error-boundary.js

--- src/components/error-component/error-boundary.js
+++ src/components/error-component/error-boundary.js
@@ -5,11 +5,18 @@ import ErrorComponent from './index';
 class ErrorBoundary extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { hasError: false };
+        this.state = {
+            hasError: false,
+        };
     }
 
     componentDidCatch = (error, info) => {
-        if (window.TrackJS) window.TrackJS.console.log(this.props.root_store);
+        console.error('🔥 ERREUR RÉELLE DE L’APPLICATION :', error);
+        console.error('🔥 INFORMATIONS :', info);
+
+        if (window.TrackJS) {
+            window.TrackJS.console.log(this.props.root_store);
+        }
 
         this.setState({
             hasError: true,
@@ -17,12 +24,23 @@ class ErrorBoundary extends React.Component {
             info,
         });
     };
-    render = () => (this.state.hasError ? <ErrorComponent should_show_refresh={true} /> : this.props.children);
+
+    render = () => {
+        if (this.state.hasError) {
+            return <ErrorComponent should_show_refresh={true} />;
+        }
+
+        return this.props.children;
+    };
 }
 
 ErrorBoundary.propTypes = {
     root_store: PropTypes.object,
-    children: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+    children: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
 };
 
 export default ErrorBoundary;
