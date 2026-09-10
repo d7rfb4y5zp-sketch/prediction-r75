d5a6d89961f2784cd9246cd0cfe9facf57572faf
d7rfb4y5zp-sketch <d7rfb4y5zp@privaterelay.appleid.com>
2026-09-10 22:07:04 +0000

    Update app-content.jsx

--- src/app/app-content.jsx
+++ src/app/app-content.jsx
@@ -2,7 +2,10 @@ import React, { lazy, Suspense, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import AuthLoadingWrapper from '@/components/auth-loading-wrapper';
-import { botNotification } from '@/components/bot-notification/bot-notification';
+import {
+    botNotification,
+    NotificationContainer,
+} from '@/components/bot-notification/bot-notification';
 import useLiveChat from '@/components/chat/useLiveChat';
 import ChunkLoader from '@/components/loader/chunk-loader';
 import { getUrlBase } from '@/components/shared';
@@ -342,6 +345,8 @@ const AppContent = observer(() => {
                         <BotStopped />
 
                         <TransactionDetailsModal />
+
+                        <NotificationContainer />
 
                     </div>
 
