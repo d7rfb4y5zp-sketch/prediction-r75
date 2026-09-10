0cbc3a3fe0e6c95e278142a780bd35857e050194
d7rfb4y5zp-sketch <d7rfb4y5zp@privaterelay.appleid.com>
2026-09-10 16:35:37 +0000

    Update app-content.jsx

--- src/app/app-content.jsx
+++ src/app/app-content.jsx
@@ -7,12 +7,7 @@ import useLiveChat from '@/components/chat/useLiveChat';
 import ChunkLoader from '@/components/loader/chunk-loader';
 import { getUrlBase } from '@/components/shared';
 
-import {
-    api_base,
-    ApiHelpers,
-    ServerTime,
-} from '@/external/bot-skeleton';
-
+import { api_base, ApiHelpers, ServerTime } from '@/external/bot-skeleton';
 import { CONNECTION_STATUS } from '@/external/bot-skeleton/services/api/observables/connection-status-stream';
 
 import { useApiBase } from '@/hooks/useApiBase';
@@ -26,6 +21,7 @@ import { setSmartChartsPublicPath } from '@deriv-com/smartcharts-champion';
 import { localize } from '@deriv-com/translations';
 
 import Main from '../pages/main';
+import BotBuilder from '../pages/bot-builder';
 
 import './app.scss';
 import '../components/bot-notification/bot-notification.scss';
@@ -36,34 +32,18 @@ const PreviewBranding =
         : null;
 
 const AppContent = observer(() => {
-    const [is_api_initialized, setIsApiInitialized] =
-        React.useState(false);
-
-    const [is_loading, setIsLoading] =
-        React.useState(true);
+    const [is_api_initialized, setIsApiInitialized] = React.useState(false);
+    const [is_loading, setIsLoading] = React.useState(true);
 
     const store = useStore();
+    const { app, transactions, common, client } = store;
 
-    const {
-        app,
-        transactions,
-        common,
-        client,
-    } = store;
+    const { recovered_transactions, recoverPendingContracts } = transactions;
 
-    const {
-        recovered_transactions,
-        recoverPendingContracts,
-    } = transactions;
+    const is_subscribed_to_msg_listener = React.useRef(false);
+    const msg_listener = React.useRef(null);
 
-    const is_subscribed_to_msg_listener =
-        React.useRef(false);
-
-    const msg_listener =
-        React.useRef(null);
-
-    const { connectionStatus } =
-        useApiBase();
+    const { connectionStatus } = useApiBase();
 
     useThemeSwitcher();
     useDevMode();
@@ -73,66 +53,39 @@ const AppContent = observer(() => {
 
         if (!process.env.NEXT_PUBLIC_DERIV_APP_ID) {
             botNotification(
-                localize(
-                    'Waiting for environment variables to be set…'
-                ),
+                localize('Waiting for environment variables to be set…'),
                 undefined,
-                {
-                    type: 'warning',
-                }
+                { type: 'warning' }
             );
         }
     }, []);
 
     const livechat_client_information = {
-        is_client_store_initialized:
-            client?.is_logged_in
-                ? true
-                : !!client,
-
-        is_logged_in:
-            client?.is_logged_in,
-
-        loginid:
-            client?.loginid,
-
-        currency:
-            client?.currency,
-
-        residence:
-            client?.residence,
-
+        is_client_store_initialized: client?.is_logged_in
+            ? true
+            : !!client,
+        is_logged_in: client?.is_logged_in,
+        loginid: client?.loginid,
+        currency: client?.currency,
+        residence: client?.residence,
         email: '',
         first_name: '',
         last_name: '',
     };
 
-    useLiveChat(
-        livechat_client_information
-    );
+    useLiveChat(livechat_client_information);
 
     useEffect(() => {
-        if (
-            connectionStatus ===
-            CONNECTION_STATUS.OPENED
-        ) {
+        if (connectionStatus === CONNECTION_STATUS.OPENED) {
             setIsApiInitialized(true);
-
             common.setSocketOpened(true);
         } else {
             common.setSocketOpened(false);
         }
-    }, [
-        common,
-        connectionStatus,
-    ]);
+    }, [common, connectionStatus]);
 
-    const {
-        current_language,
-    } = common;
-
-    const html =
-        document.documentElement;
+    const { current_language } = common;
+    const html = document.documentElement;
 
     React.useEffect(() => {
         html?.setAttribute(
@@ -142,52 +95,39 @@ const AppContent = observer(() => {
 
         html?.setAttribute(
             'dir',
-            current_language.toLowerCase() ===
-                'ar'
+            current_language.toLowerCase() === 'ar'
                 ? 'rtl'
                 : 'ltr'
         );
-    }, [
-        current_language,
-        html,
-    ]);
+    }, [current_language, html]);
 
-    const handleMessage =
-        React.useCallback(
-            ({ data }) => {
+    const handleMessage = React.useCallback(
+        ({ data }) => {
+            if (
+                data?.msg_type === 'proposal_open_contract' &&
+                !data?.error
+            ) {
+                const { proposal_open_contract } = data;
+
                 if (
-                    data?.msg_type ===
-                        'proposal_open_contract' &&
-                    !data?.error
+                    proposal_open_contract?.status !== 'open' &&
+                    !recovered_transactions?.includes(
+                        proposal_open_contract?.contract_id
+                    )
                 ) {
-                    const {
-                        proposal_open_contract,
-                    } = data;
-
-                    if (
-                        proposal_open_contract?.status !==
-                            'open' &&
-                        !recovered_transactions?.includes(
-                            proposal_open_contract?.contract_id
-                        )
-                    ) {
-                        recoverPendingContracts(
-                            proposal_open_contract
-                        );
-                    }
+                    recoverPendingContracts(proposal_open_contract);
                 }
-            },
-            [
-                recovered_transactions,
-                recoverPendingContracts,
-            ]
-        );
+            }
+        },
+        [
+            recovered_transactions,
+            recoverPendingContracts,
+        ]
+    );
 
     React.useEffect(() => {
         setSmartChartsPublicPath(
-            getUrlBase(
-                '/js/smartcharts/'
-            )
+            getUrlBase('/js/smartcharts/')
         );
     }, []);
 
@@ -198,15 +138,11 @@ const AppContent = observer(() => {
             is_api_initialized &&
             api_base?.api
         ) {
-            is_subscribed_to_msg_listener.current =
-                true;
+            is_subscribed_to_msg_listener.current = true;
 
-            msg_listener.current =
-                api_base.api
-                    .onMessage()
-                    ?.subscribe(
-                        handleMessage
-                    );
+            msg_listener.current = api_base.api
+                .onMessage()
+                ?.subscribe(handleMessage);
         }
 
         return () => {
@@ -214,9 +150,7 @@ const AppContent = observer(() => {
                 is_subscribed_to_msg_listener.current &&
                 msg_listener.current
             ) {
-                is_subscribed_to_msg_listener.current =
-                    false;
-
+                is_subscribed_to_msg_listener.current = false;
                 msg_listener.current.unsubscribe?.();
             }
         };
@@ -237,56 +171,39 @@ const AppContent = observer(() => {
             app.api_helpers_store
         );
 
-        import('@/utils/gtm').then(
-            ({ default: GTM }) => {
-                GTM.init(store);
-            }
-        );
+        import('@/utils/gtm').then(({ default: GTM }) => {
+            GTM.init(store);
+        });
     };
 
-    const changeActiveSymbolLoadingState =
-        () => {
-            init();
+    const changeActiveSymbolLoadingState = () => {
+        init();
 
-            const retrieveActiveSymbols =
-                () => {
-                    const {
-                        active_symbols,
-                    } =
-                        ApiHelpers.instance;
+        const retrieveActiveSymbols = () => {
+            const { active_symbols } =
+                ApiHelpers.instance;
 
-                    active_symbols
-                        .retrieveActiveSymbols(
-                            true
-                        )
-                        .then(() => {
-                            setIsLoading(
-                                false
-                            );
-                        });
-                };
-
-            if (
-                ApiHelpers?.instance
-                    ?.active_symbols
-            ) {
-                retrieveActiveSymbols();
-            } else {
-                const intervalId =
-                    setInterval(() => {
-                        if (
-                            ApiHelpers?.instance
-                                ?.active_symbols
-                        ) {
-                            clearInterval(
-                                intervalId
-                            );
-
-                            retrieveActiveSymbols();
-                        }
-                    }, 1000);
-            }
+            active_symbols
+                .retrieveActiveSymbols(true)
+                .then(() => {
+                    setIsLoading(false);
+                });
         };
+
+        if (ApiHelpers?.instance?.active_symbols) {
+            retrieveActiveSymbols();
+        } else {
+            const intervalId = setInterval(() => {
+                if (
+                    ApiHelpers?.instance
+                        ?.active_symbols
+                ) {
+                    clearInterval(intervalId);
+                    retrieveActiveSymbols();
+                }
+            }, 1000);
+        }
+    };
 
     React.useEffect(() => {
         if (is_api_initialized) {
@@ -300,9 +217,7 @@ const AppContent = observer(() => {
         }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [
-        is_api_initialized,
-    ]);
+    }, [is_api_initialized]);
 
     React.useEffect(() => {
         if (
@@ -318,9 +233,7 @@ const AppContent = observer(() => {
         client.loginid,
     ]);
 
-    if (common?.error) {
-        return null;
-    }
+    if (common?.error) return null;
 
     return (
         <React.Fragment>
@@ -348,6 +261,8 @@ const AppContent = observer(() => {
                     >
 
                         <Main />
+
+                        <BotBuilder />
 
                     </div>
 
