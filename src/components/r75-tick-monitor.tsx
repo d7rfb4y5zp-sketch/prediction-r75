e4b0e4430b8d9eb965c0713a1a446f91fe8737f3
d7rfb4y5zp-sketch <d7rfb4y5zp@privaterelay.appleid.com>
2026-09-17 23:52:55 +0000

    Create r75-tick-monitor.tsx

---
+++ src/components/r75-tick-monitor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const R75TickMonitor = () => {
+    return (
+        <div>
+            <h2>R75 Tick Monitor</h2>
+            <p>Connexion R_75 en préparation…</p>
+        </div>
+    );
+};
+
+export default R75TickMonitor;
